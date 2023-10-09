d437c2d2c6a4488a5d508dbf660c609c1884916c
Xaqani Mamedov <hagani24680@gmail.com>
2023-10-09 14:17:40 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/main.css";
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#root").innerHTML = "Hello";
+});
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { type } = require("os");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "loanica",
+      template: path.resolve(__dirname, "./src/template.html"),
+      filename: "index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  module: {
+    rules: [
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: "asset/resource" },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+
+  mode: "development",
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+};
